fdeae2c86278c9c2e67a069e572483bd6f141a05
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-10 15:27:40 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-#include <stdio.h> int main(){puts("Programming is like building a multilingual puzzle");return 0;}
+#include <stdio.h>
+/**
+ * main : all functions in it
+ * return : end with 0
+ **/
+int main()
+{
+	puts("Programming is like building a multilingual puzzle");
+	return 0;
+}
