c83a053d10d8089dce79708e022cb36e39fdbc14
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-10 17:32:06 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stido.h>
+/**
+ * main - main function
+ *
+ * Return: return 0
+ * */
+int main(void)
+{
+printf("and that piece of art is useful" - Dora Korpar, 2015-10-19");
+return (0);
+}
