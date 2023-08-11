c34cf46e1aed8a0b6edb8f4ab08dcfb1e3f2bc7f
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-11 16:43:24 -0700

    call the alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+*main - main function
+*Return: return 0
+*Discription: 'alphabet game'
+*/
+int main(void)
+{
+int no = 97;
+for (no = 98; no <= 122; no++)
+{
+putchar(no);
+}
+putchar('\n');
+return (0);
+}
