2f0411489baf671fe8b7ffe00e17f5474b892f4a
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-11 18:51:31 -0700

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Description: 'numbers of base 16'
+ * Return: return 0
+ */
+int main(void)
+{
+int num;
+int n;
+for (num = 48; num <= 57; num++)
+{
+putchar(num);
+}
+for (n = 97; n <= 102; n++)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
