8af7b2b84e4fffdba0bb979be01ffbb5f073133f
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-11 18:37:11 -0700

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Discription: 'all alphabet in reverse'
+ * Return: return 0
+ */
+int main(void)
+{
+int n;
+for (n = 122; n >= 97; n--)
+{
+putchar(n);
+}
+putchar('\n');
+return (0);
+}
