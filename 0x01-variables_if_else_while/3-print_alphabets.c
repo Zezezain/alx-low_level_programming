af107d0d6efadcf5584595804efc8a2013931675
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-11 17:07:42 -0700

    call the alphabet game all letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * Return: return o
+ * Discription: 'alphabet game with all letters'
+ */
+int main(void)
+{
+int no;
+int noo;
+for (no = 97; no <= 122; no++)
+{
+putchar(no);
+}
+for (noo = 65; noo <= 90; noo++)
+{
+putchar(noo);
+}
+putchar('\n');
+return (0);
+}
