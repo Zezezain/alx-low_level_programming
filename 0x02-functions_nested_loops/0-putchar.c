2508017e625aa6f9b89d19a3d4f9386ff6330a43
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-15 16:24:46 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+int z;
+char c[] = "_putchar";
+for (z = 0; z  <= 7; z++)
+{
+putchar(c[z]);
+}
+putchar('\n');
+return (0);
+}
