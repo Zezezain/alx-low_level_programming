8ed4f29d65d818dc5fca94e1bb6dab75303ef3e9
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-16 07:27:29 -0700

    addition

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * add - sum tow numbers
+ *
+ * Discription: sum two numbers
+ * @a: the first number
+ * @b: the second one
+ * Return: return 0
+ */
+int add(int a, int b)
+{
+int r = a + b;
+return (r);
+}
