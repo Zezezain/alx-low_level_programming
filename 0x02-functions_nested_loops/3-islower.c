13b330c03ab0e43bdadc9a815988944ce7315cd7
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-15 18:46:53 -0700

    detecting alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -5,12 +5,12 @@
  * Discription: by using _putchar
  * @c: ditict alphabets
  *
- * Return: 1 if correct
+ * Return: 1 if correct 0 if not
  */
 int _islower(int c)
 {
 char ph;
-int l;
+int l = 0;
 for (ph = 97; ph <= 122; ph++)
 {
 if (ph == c)
