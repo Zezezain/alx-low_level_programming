66b747c8051ab97feaf887fc573669d483abe9d2
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-17 18:33:24 -0700

    The numbers speak for themselves

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_numbers - list from 0 to 9
+ *
+ *Return: no return
+ */
+
+void print_numbers(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+_putchar(a);
+}
+_putchar('\n');
+}
