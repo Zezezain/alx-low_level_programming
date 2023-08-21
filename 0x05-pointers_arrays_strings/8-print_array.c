514502fa3a4baaf131224dac613cfbeff1e2abe5
Zezezain <zeinab.zain.zz@gmail.com>
2023-08-21 15:31:59 -0700

    array

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -11,9 +11,9 @@ void print_array(int *a, int n)
 int b;
 for (b = 0; b < (n - 1); b++)
 {
-printf("%d, ", a[n]);
+printf("%d, ", a[b]);
 }
-if (n == (n - 1))
+if (b == (n - 1))
 {
 printf("%d", a[n - 1]);
 }
