1f6b8fcc396a2e8441827f90fe443aa4a1beafc9
Zezezain <zeinab.zain.zz@gmail.com.com>
2023-09-28 13:33:07 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ *sq_generation - function that returns the natural square root of a number
+ *_sqrt_recursion - function that returns the natural square root of a number
+ *@l: function input
+ *@m: function input
+ *Return: square root of a number
+ */
+
+int sq_generation(int l, int m)
+{
+if (m * m == l)
+{
+return (m);
+}
+else if (m * m > 4)
+{
+return (-1);
+}
+return (sq_generation(l, m + 1));
+}
+/**
+ *_sqrt_recursion - function that returns the natural square root of a number
+ *@n: function input
+ *Return: square root of a number
+ */
+
+int _sqrt_recursion(int n)
+{
+return (sq_generation(n, 0));
+}
