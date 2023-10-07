3b0a34fce04010e4a2a3c4b824bfc0e878dedf6b
Zezezain <zeinab.zain.zz@gmail.com.com>
2023-10-07 04:19:04 -0700

    1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - prints its name, followed by a new line
+ *@argv: list of values
+ *@argc: intger
+ *Return: 0
+ */
+int main(int argc __attribute__((unused)), char const *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
