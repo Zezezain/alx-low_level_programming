214a29104ff322682470c2a2961174eb601e338b
Zezezain <zeinab.zain.zz@gmail.com.com>
2023-10-15 08:49:47 -0700

    1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOC_H
+#define DOC_H
+
+/**
+ * struct dog - a dog struct
+ * @age: age of dog
+ * @owner: name of owner
+ * @name:name of dog
+ *Description: alone dog struct in a big kitty world
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
