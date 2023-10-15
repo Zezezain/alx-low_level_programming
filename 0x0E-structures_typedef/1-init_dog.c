2111350d2aa8de5db474fdd3f5df3feb315878a6
Zezezain <zeinab.zain.zz@gmail.com.com>
2023-10-15 09:07:12 -0700

    4

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,7 +4,8 @@
  *init_dog - initializing a dog
  *@d: the dog init
  *@name:the name of dog
- *@age: the name of owner
+ *@age: the age of dog
+ *@owner: the name of owner
  *Return: void
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
