b6b52179290544b5d0ebf18f9882572c0b96a0c9
Zezezain <zeinab.zain.zz@gmail.com>
2023-10-27 06:31:29 -0700

    zoba

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,11 +1,13 @@
+#include "main.h"
 #include <stdio.h>
+
 /**
- *main -  prints the number of arguments passed into it
+ *main - prints the number of arguments passed into it
  *@argc: number of command line
- *@argv: array of that contain the program
+ *@argv: argument vector
  *Return: 0
  */
-int main(int argc, char **argv __attribute__((unused))))
+int main(int argc, char *argv[])
 {
 printf("%d\n", argc - 1);
 return (0);
